dd830d1c5d2c702d68030c9a5980d260c3ccaee8
Scott Winters <Scottwinters169@air-watch.com>
2014-06-19 13:59:37 -0400

    push project

---
+++ learning/learning/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  learning
+//
+//  Created by Scott Winters on 6/19/14.
+//  Copyright (c) 2014 Scott Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
